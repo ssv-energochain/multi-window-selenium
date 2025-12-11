4c24975480435130d057f317c86628d76da05772
Сергей Сереженькин <sv.serezhenkin@gmail.com>
2025-12-11 16:40:50 +0300

    upd readme

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
-set URL=https://www.twitch.tv/konstantinbestgames
+set URL=https://www.google.com
 set count=1
-set browser=firefox
+set browser=yandex
 python E:\00_WORK\EnergoChain\multi-window-selenium\main.py -u %URL% -c %count% -b %browser%
 pause
 
